532c6d96d5898f6487fda33d33e2db6bd19b5ee2
betadiaz326 <diazbetania326@gmail.com>
2022-05-15 00:25:08 -0300

    cree carpeta

---
+++ BACKEND/beta/src/test/java/com/portfolio/beta/BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
