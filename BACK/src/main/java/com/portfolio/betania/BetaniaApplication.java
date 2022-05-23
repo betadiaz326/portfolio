a4f29c429f50b69fee88a9a067136e81b21b2d61
betadiaz326 <diazbetania326@gmail.com>
2022-05-22 22:46:14 -0300

    cambios

---
+++ BACK/src/main/java/com/portfolio/betania/BetaniaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.betania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaniaApplication.class, args);
+	}
+
+}
---
+++ BACK/src/test/java/com/portfolio/betania/BetaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.betania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
