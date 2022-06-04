694c889453b0a590500cca3c7b8760e0e8f2df49
betadiaz326 <diazbetania326@gmail.com>
2022-06-03 22:58:01 -0300

    se crearon archivos

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FooterComponent } from './components/footer/footer.component' ;
 import {HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     NgCircleProgressModule . forRoot ( {}),
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
